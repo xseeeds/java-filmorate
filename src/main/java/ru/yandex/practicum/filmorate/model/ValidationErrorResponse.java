3919dbac0c7d93ecb18af8512556c5e392ac9e22
xseeeds <s86ko@yandex.ru>
2023-03-27 07:24:02 +0400

    ТЗ №10

--- src/main/java/ru/yandex/practicum/filmorate/model/ValidationErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/ValidationErrorResponse.java
@@ -9,6 +9,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ValidationErrorResponse {
 
-    private final List<Violation> Violations;
+    private final List<Violation> violations;
 
 }
